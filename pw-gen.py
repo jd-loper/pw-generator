669c6dbcacb0c30581601696fdb4966e0a339331
Joseph Loper <jd.loper@icloud.com>
2023-04-24 10:51:40 -0700

    Add more notes

--- pw-gen.py
+++ pw-gen.py
@@ -1,40 +1,40 @@
-import random
-from tkinter import *
+import random  # Enables random selection of characters
+from tkinter import *  # Enables GUI
 import string
-from tkinter.font import Font
+from tkinter.font import Font  # Enables font selection
 import pyperclip  # Enables copy/paste functionality
 
 
 def generate_password():  # Selects 4 random characters
-    password = []
+    password = []  # Creates empty list
     for i in range(4):
-        alpha = random.choice(string.ascii_letters)
-        symbol = random.choice(string.punctuation)
-        numbers = random.choice(string.digits)
-        password.append(alpha)
-        password.append(symbol)
-        password.append(numbers)
-    y = "".join(str(x)for x in password)
-    lbl.config(text=y)
-    pyperclip.copy(y)
+        alpha = random.choice(string.ascii_letters)  # Selects random letter
+        symbol = random.choice(string.punctuation)  # Selects random symbol
+        numbers = random.choice(string.digits)  # Selects random number
+        password.append(alpha)  # Adds random character to list
+        password.append(symbol)  # Adds random character to list
+        password.append(numbers)  # Adds random character to list
+    y = "".join(str(x) for x in password)  # Converts list to string
+    lbl.config(text=y)  # Displays password
+    pyperclip.copy(y)  # Copies password to clipboard
 
 
 def copy_password():
-    password = lbl.cget("text")
-    pyperclip.copy(password)
+    password = lbl.cget("text")  # Gets password from label
+    pyperclip.copy(password)  # Copies password to clipboard
 
 
 root = Tk()  # Creates GUI window
 root.geometry("250x200")  # Window dimensions
 
 btn = Button(root, text="Generate Password", command=generate_password)
-btn.place(relx=0.5, rely=0.2, anchor=N)
+btn.place(relx=0.5, rely=0.2, anchor=N)  # Places button
 
 btn_copy = Button(root, text="Copy Password", command=copy_password)
 btn_copy.place(relx=0.5, rely=0.4, anchor=N)
 
-myFont = Font(family="Courier New", size=14)
-lbl = Label(root, font=myFont)
-lbl.place(relx=0.5, rely=0.6, anchor=CENTER)
+myFont = Font(family="Courier New", size=14)  # Creates font
+lbl = Label(root, font=myFont)  # Creates label
+lbl.place(relx=0.5, rely=0.6, anchor=CENTER)  # Places label
 
-root.mainloop()
+root.mainloop()  # Runs GUI
